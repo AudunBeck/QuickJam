1606840132cad5de0b04d6cf5b6365a11ab0df3b
Audun - Skole PC <audunking@gmail.com>
2018-12-14 11:47:42 +0100

    Initial Commit

---
+++ Source/QuickJam/QuickJam.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "QuickJam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, QuickJam, "QuickJam" );
